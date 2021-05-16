82e5774f18fd852d0ffa9a9310e71e4c2b701a33
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2021-05-16 12:06:40 +0700

    Add files via upload

--- javasc.js
+++ javasc.js
@@ -3,16 +3,8 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-function mid1Function() {
-    document.documentElement.scrollTop = 200;
-}
-
 function mid2Function() {
     document.documentElement.scrollTop = 1650;
-}
-
-function mid3Function() {
-    document.documentElement.scrollTop = 13500;
 }
 
 function bottomFunction() {
